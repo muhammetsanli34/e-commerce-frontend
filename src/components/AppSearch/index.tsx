d4a7d98b0e00d5c1d7e2d742ba71960bc732ea70
Muhammet Emin Şanlı <muhammetsanli34@gmail.com>
2024-07-13 15:50:52 +0300

    Added mock categories to AppSelect options

--- src/components/AppSearch/index.tsx
+++ src/components/AppSearch/index.tsx
@@ -2,12 +2,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import style from "./style.module.scss";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import AppSelect from "../AppSelect";
+import categories from "@/src/mock/categories";
 
 export default function AppSearch() {
   return (
     <div className={style.appSearch}>
       <AppSelect
-        options={["All", "Title", "Author"]}
+        options={categories}
         value="All"
         onChange={() => {}}
       />
---
+++ src/mock/categories.ts
@@ -0,0 +1,14 @@
+export default [
+    "All",
+    "Baby & Kids",
+    "Beauty & Personal Care",
+    "Books",
+    "Clothing, Shoes & Jewelry",
+    "Electronics",
+    "Grocery & Gourmet Food",
+    "Health, Household & Baby Care",
+    "Home & Kitchen",
+    "Movies & TV",
+    "Music, CDs & Vinyl",
+    "Pet Supplies",
+]
