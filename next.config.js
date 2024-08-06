6078acbd964cfe9b38f2ca4728e37091407d7135
Muhammet Emin Şanlı <muhammetsanli34@gmail.com>
2024-08-06 21:32:59 +0300

    sass files moved to styles folder under the src

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ const path = require("path");
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
-    prependData: `@import "@/src/app/variables.scss";`,
+    prependData: `@import "@/src/style/variables.scss"; @import "@/src/style/mixins.scss";`,
 
   },
   images: {
     domains: ["klbtheme.com"],
-  }
+  },
 };
 
 module.exports = nextConfig;
